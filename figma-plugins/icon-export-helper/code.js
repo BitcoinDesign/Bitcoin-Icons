1084b0ba6e62f7650fb140c27e4034904a9388e7
GBKS <chri@sto.ph>
2021-03-22 15:14:34 +0100

    Code update

--- figma-plugins/icon-export-helper/code.js
+++ figma-plugins/icon-export-helper/code.js
@@ -4,57 +4,76 @@ page.name = "Icons for export";
 figma.root.appendChild(page);
 // Get all nodes that are components and begin with "Icon/"
 const nodes = figma.currentPage.findAll(node => node.name.indexOf('Icon/') === 0 && node.type == 'COMPONENT_SET');
-// console.log('nodes', nodes);
 const newNodes = [];
 let x = 0;
 let y = 0;
 // Create instance of each component and variant
 for (const node of nodes) {
-    // console.log('node', node);
+    // Only use components with variants.
     if (node.type === 'COMPONENT_SET') {
+        // Loop all component variants.
         for (const nodeVariant of node.children) {
             if (nodeVariant.type === 'COMPONENT') {
-                // console.log('nodeVariant', nodeVariant)
-                const nodeInstance = nodeVariant.createInstance();
-                // console.log('nodeInstance', nodeInstance)
                 if (nodeVariant.type === 'COMPONENT') {
-                    // console.log('mainComponent', nodeInstance.mainComponent.name)
-                    // console.log('masterComponent', nodeInstance.masterComponent)
-                    // for(const testo in nodeInstance.mainComponent) {
-                    // 	console.log(testo, nodeInstance.mainComponent[testo])
-                    // }
-                    nodeInstance.x += x * 40;
-                    nodeInstance.y += y * 40;
-                    let newName = node.name.substr(5).toLowerCase().replace(' ', '_');
-                    const variantNameBits = nodeInstance.mainComponent.name.split(', ');
+                    let exportIcon = false;
+                    // Variant settings are stored in the instance name.
+                    // Use them to figure out whether to export and into which folder.
+                    // "Style=Filled, Size=Big"
+                    const variantNameBits = nodeVariant.name.split(', ');
+                    let subFolder = '';
                     for (const bit of variantNameBits) {
-                        newName += '-' + bit.split('=')[1];
-                    }
-                    newName = newName.toLowerCase().replace(' ', '-');
-                    nodeInstance.name = newName;
-                    nodeInstance.exportSettings = [
-                        {
-                            contentsOnly: true,
-                            format: 'SVG',
-                            suffix: '',
-                            svgIdAttribute: false,
-                            svgOutlineText: true,
-                            svgSimplifyStroke: true
+                        const subBits = bit.split('=');
+                        switch (subBits[0]) {
+                            case 'Style':
+                                // Use the style name as folder name
+                                subFolder = subBits[1] + '/';
+                                break;
+                            case 'Size':
+                                // Only export Medium sizes for now
+                                exportIcon = subBits[1] == 'Medium';
+                                break;
                         }
-                    ];
-                    page.appendChild(nodeInstance);
-                    newNodes.push(nodeInstance);
-                    x++;
-                    if (x >= 10) {
-                        x = 0;
-                        y += 1;
+                    }
+                    if (exportIcon === true) {
+                        const nodeInstance = nodeVariant.createInstance();
+                        // Position nicely, assuming a 40x40 grid.
+                        nodeInstance.x += x * 40;
+                        nodeInstance.y += y * 40;
+                        // Finalize name.
+                        // "Icon/Bitcoin circle" with variant "Style=Outline" becomes
+                        // "outline/bitcoin-circle"
+                        let newName = subFolder + node.name.split('/')[1];
+                        newName = newName.toLowerCase().replace(' ', '-');
+                        nodeInstance.name = newName;
+                        nodeInstance.exportSettings = [
+                            {
+                                contentsOnly: true,
+                                format: 'SVG',
+                                suffix: '',
+                                svgIdAttribute: false,
+                                svgOutlineText: true,
+                                svgSimplifyStroke: true
+                            }
+                        ];
+                        // Add instance to our new page.
+                        page.appendChild(nodeInstance);
+                        // Keep track of instances for selection.
+                        newNodes.push(nodeInstance);
+                        x++;
+                        // Reset the row every 10 icons
+                        if (x >= 10) {
+                            x = 0;
+                            y += 1;
+                        }
                     }
                 }
             }
         }
     }
 }
+// Select our new instances for easy export.
 page.selection = newNodes;
+// Go to our new page.
 figma.currentPage = page;
 // Make sure to close the plugin when you're done. Otherwise the plugin will
 // keep running, which shows the cancel button at the bottom of the screen.
--- figma-plugins/icon-export-helper/code.ts
+++ figma-plugins/icon-export-helper/code.ts
@@ -1,5 +1,4 @@
 // Create new page
-
 const page = figma.createPage()
 page.name = "Icons for export"
 
@@ -8,7 +7,6 @@ figma.root.appendChild(page);
 // Get all nodes that are components and begin with "Icon/"
 const nodes = figma.currentPage.findAll(node => node.name.indexOf('Icon/') === 0 && node.type == 'COMPONENT_SET')
 
-// console.log('nodes', nodes);
 const newNodes = []
 
 let x = 0
@@ -16,46 +14,73 @@ let y = 0
 
 // Create instance of each component and variant
 for (const node of nodes) {
+	// Only use components with variants.
 	if(node.type === 'COMPONENT_SET') {
 
+		// Loop all component variants.
 		for(const nodeVariant of node.children) {
 			if(nodeVariant.type === 'COMPONENT') {
-				const nodeInstance = nodeVariant.createInstance()
-
 				if(nodeVariant.type === 'COMPONENT') {
-					nodeInstance.x += x * 40
-					nodeInstance.y += y * 40
+					let exportIcon = false
 
-					let newName = node.name.substr(5).toLowerCase().replace(' ', '_')
-
-					const variantNameBits = nodeInstance.mainComponent.name.split(', ')
+					// Variant settings are stored in the instance name.
+					// Use them to figure out whether to export and into which folder.
+					// "Style=Filled, Size=Big"
+					const variantNameBits = nodeVariant.name.split(', ')
+					let subFolder = ''
 					for(const bit of variantNameBits) {
-						newName += '-' + bit.split('=')[1]
+						const subBits = bit.split('=')
+
+						switch(subBits[0]) {
+							case 'Style':
+								// Use the style name as folder name
+								subFolder = subBits[1] + '/'
+								break;
+							case 'Size':
+								// Only export Medium sizes for now
+								exportIcon = subBits[1] == 'Medium'
+								break;
+						}
 					}
 
-					newName = newName.toLowerCase().replace(' ', '-')
-					nodeInstance.name = newName
+					if(exportIcon === true) {
+						const nodeInstance = nodeVariant.createInstance()
 
-					nodeInstance.exportSettings = [
-						{
-							contentsOnly: true,
-							format: 'SVG',
-							suffix: '',
-							svgIdAttribute: false,
-							svgOutlineText: true,
-							svgSimplifyStroke: true
+						// Position nicely, assuming a 40x40 grid.
+						nodeInstance.x += x * 40
+						nodeInstance.y += y * 40
+
+						// Finalize name.
+						// "Icon/Bitcoin circle" with variant "Style=Outline" becomes
+						// "outline/bitcoin-circle"
+						let newName = subFolder + node.name.split('/')[1]
+						newName = newName.toLowerCase().replace(' ', '-')
+						nodeInstance.name = newName
+
+						nodeInstance.exportSettings = [
+							{
+								contentsOnly: true,
+								format: 'SVG',
+								suffix: '',
+								svgIdAttribute: false,
+								svgOutlineText: true,
+								svgSimplifyStroke: true
+							}
+						]
+
+						// Add instance to our new page.
+						page.appendChild(nodeInstance)
+
+						// Keep track of instances for selection.
+						newNodes.push(nodeInstance)
+
+						x++
+
+						// Reset the row every 10 icons
+						if(x >= 10) {
+							x = 0
+							y += 1
 						}
-					]
-
-					page.appendChild(nodeInstance)
-
-					newNodes.push(nodeInstance)
-
-					x++
-
-					if(x >= 10) {
-						x = 0
-						y += 1
 					}
 				}
 			}
@@ -63,7 +88,10 @@ for (const node of nodes) {
 	}
 }
 
+// Select our new instances for easy export.
 page.selection = newNodes
+
+// Go to our new page.
 figma.currentPage = page
 
 // Make sure to close the plugin when you're done. Otherwise the plugin will
